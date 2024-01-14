10cfea70bdd3ffb824f648626c5e3399a397ee00
Gabriela-M-Silva <gabrielademoraes@yahoo.com>
2024-01-14 15:45:24 -0300

    linha apos construtor em nokia e iphone

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -5,6 +5,7 @@ namespace DesafioPOO.Models
         public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
         {
         }
+        
         public override void InstalarAplicativo(string nomeApp)
         {
             Console.WriteLine($"Instalando o aplicativo \"{nomeApp}\" no Iphone");
