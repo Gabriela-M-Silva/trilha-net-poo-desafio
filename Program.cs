71f6826e1808af9a6873c8f4a283a124cb35b45d
Gabriela-M-Silva <gabrielademoraes@yahoo.com>
2024-01-14 15:43:16 -0300

    implementacoes realizadas

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Testando o Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "12345", modelo: "Modelo 1", imei: "000011", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Skeelo");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Testando o Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "54321", modelo: "Modelo 2", imei: "110000", memoria: 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("ReadEra");
